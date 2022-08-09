05f613431a556083c56cfed73238f8cb25f31039
tlips <thomas.lips@ugent.be>
2022-08-09 16:34:12 +0200

    initial structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # representations-from-demonstrations
+
+## installation
+- create conda env `conda create -n rfd python=3.9`
+- `conda env update -f environment.yaml`
---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="rfd",
+    version="0.0.1",
+    author="Thomas Lips",
+    author_email="thomas.lips@ugent.be",
+    description="learning representations from demonstrations for robot learning",
+    packages=["rfd"],
+)
